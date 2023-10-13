73a0b261dbf8c44f1059eea3254a51845f8c66cb
Frihat <frihatmohamed@hotmail.fr>
2023-10-13 07:35:45 +0200

    fix compilation issue

--- contracts/interfaces/ITapETH.sol
+++ contracts/interfaces/ITapETH.sol
@@ -3,58 +3,56 @@ pragma solidity ^0.8.18;
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
 interface ITapETH is IERC20 {
-  function proposeGovernance(address _governance) external;
+    function proposeGovernance(address _governance) external;
 
-  function acceptGovernance() external;
+    function acceptGovernance() external;
 
-  function addPool(address _pool) external;
+    function addPool(address _pool) external;
 
-  function removePool(address _pool) external;
+    function removePool(address _pool) external;
 
-  function getTotalPooledEther() external view returns (uint256);
+    function getTotalPooledEther() external view returns (uint256);
 
-  function increaseAllowance(
-    address _spender,
-    uint256 _addedValue
-  ) external returns (bool);
+    function increaseAllowance(
+        address _spender,
+        uint256 _addedValue
+    ) external returns (bool);
 
-  function decreaseAllowance(
-    address _spender,
-    uint256 _subtractedValue
-  ) external returns (bool);
+    function decreaseAllowance(
+        address _spender,
+        uint256 _subtractedValue
+    ) external returns (bool);
 
-  function getTotalShares() external view returns (uint256);
+    function getTotalShares() external view returns (uint256);
 
-  function sharesOf(address _account) external view returns (uint256);
+    function sharesOf(address _account) external view returns (uint256);
 
-  function getSharesByPooledEth(
-    uint256 _ethAmount
-  ) external view returns (uint256);
+    function getSharesByPooledEth(
+        uint256 _ethAmount
+    ) external view returns (uint256);
 
-  function setTotalSupply(uint256 _amount) external;
+    function setTotalSupply(uint256 _amount) external;
 
-  function setBuffer(uint256 _amount) external;
+    function setBuffer(uint256 _amount) external;
 
-  function rebase() external returns (uint256);
+    function getPooledEthByShares(
+        uint256 _sharesAmount
+    ) external view returns (uint256);
 
-  function getPooledEthByShares(
-    uint256 _sharesAmount
-  ) external view returns (uint256);
+    function transferShares(
+        address _recipient,
+        uint256 _sharesAmount
+    ) external returns (uint256);
 
-  function transferShares(
-    address _recipient,
-    uint256 _sharesAmount
-  ) external returns (uint256);
+    function transferSharesFrom(
+        address _sender,
+        address _recipient,
+        uint256 _sharesAmount
+    ) external returns (uint256);
 
-  function transferSharesFrom(
-    address _sender,
-    address _recipient,
-    uint256 _sharesAmount
-  ) external returns (uint256);
+    function mintShares(address _account, uint256 _sharesAmount) external;
 
-  function mintShares(address _account, uint256 _sharesAmount) external;
+    function burnShares(uint256 _sharesAmount) external;
 
-  function burnShares(uint256 _sharesAmount) external;
-
-  function burnSharesFrom(address _account, uint256 _sharesAmount) external;
+    function burnSharesFrom(address _account, uint256 _sharesAmount) external;
 }
