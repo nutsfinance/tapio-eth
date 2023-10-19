c3b574c5483f3664aa3b09e44cfbe457701ad996
Frank Yin <ukby.1234@gmail.com>
2023-10-19 13:16:27 -0700

    deploy scripts

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -14,29 +14,28 @@ async function main() {
   const wETHAddress = '0xB4FBF271143F4FBf7B91A5ded31805e42b2208d6';
   const stETHAddress = '0x1643E812aE58766192Cf7D2Cf9567dF2C37e9B7F';
   const rETHAddress = '0x178e141a0e3b34152f73ff610437a7bf9b83267a';
-  const feeAddress = '0x3a4ABb0eE1dE2aCcDFE14b80B4DEe78F983b3dcF';
-  const yieldAddress = '0xDeEc86988C66618e574ed1eFF2C5CA5745d2916d';
 
-  const ConstantExchangeRateProvider = await ethers.getContractFactory("ConstantExchangeRateProvider");
-  const StableAssetToken = await ethers.getContractFactory("StableAssetToken");
+  const TapETH = await ethers.getContractFactory("TapETH");
+  const WTapETH = await ethers.getContractFactory("WTapETH");
   const StableAsset = await ethers.getContractFactory("StableAsset");
-  const constant = await ConstantExchangeRateProvider.deploy();
-  const RocketTokenExchangeRateProvider = await ethers.getContractFactory("RocketTokenExchangeRateProvider");
-  const rate = await upgrades.deployProxy(RocketTokenExchangeRateProvider, [rETHAddress]);
-  console.log(`constant: ${constant.address}`);
-  console.log(`rETH Rate: ${rate.address}`);
+  const constantAddress = '0x07e70721C1737a9D410bcd038BA7e82e8BC19e2a';
+  const rocketRateAddress = '0xf2dD62922B5f0cb2a72dAeda711018d6F56EEb17';
 
-  const poolToken = await upgrades.deployProxy(StableAssetToken, ["Tapio Ether", "tapETH"]);
+  const poolToken = await upgrades.deployProxy(TapETH, [], {timeout: 600000});
   console.log("poolToken deployed");
   console.log(`poolToken: ${poolToken.address}`);
 
-  const stETHSwap = await upgrades.deployProxy(StableAsset, [[wETHAddress, stETHAddress], [PRECISION, PRECISION], [MINT_FEE, SWAP_FEE, REDEEM_FEE], feeAddress, yieldAddress, poolToken.address, INITIAL_A, constant.address, 1]);
-  const rETHSwap = await upgrades.deployProxy(StableAsset, [[wETHAddress, rETHAddress], [PRECISION, PRECISION], [MINT_FEE, SWAP_FEE, REDEEM_FEE], feeAddress, yieldAddress, poolToken.address, INITIAL_A, rate.address, 1]);
+  const wrappedPoolToken = await upgrades.deployProxy(WTapETH, [poolToken.address], {timeout: 600000});
+  console.log("wrapped poolToken deployed");
+  console.log(`wrappedPoolToken: ${wrappedPoolToken.address}`);
+
+  const stETHSwap = await upgrades.deployProxy(StableAsset, [[wETHAddress, stETHAddress], [PRECISION, PRECISION], [MINT_FEE, SWAP_FEE, REDEEM_FEE], poolToken.address, INITIAL_A, constantAddress, 1], {timeout: 600000});
+  const rETHSwap = await upgrades.deployProxy(StableAsset, [[wETHAddress, rETHAddress], [PRECISION, PRECISION], [MINT_FEE, SWAP_FEE, REDEEM_FEE], poolToken.address, INITIAL_A, rocketRateAddress, 1], {timeout: 600000});
   console.log(`stETHSwap: ${stETHSwap.address}`);
   console.log(`rETHSwap: ${rETHSwap.address}`);
   
-  await poolToken.setMinter(stETHSwap.address, true);
-  await poolToken.setMinter(rETHSwap.address, true);
+  await poolToken.addPool(stETHSwap.address);
+  await poolToken.addPool(rETHSwap.address);
   // await stETHSwap.unpause();
   // await cbETHSwap.unpause();
   console.log("Approval compelted");
--- scripts/mint.ts
+++ scripts/mint.ts
@@ -10,8 +10,8 @@ async function main() {
   const [deployer] = await ethers.getSigners();
   console.log(deployer.address);
   const StableAssetApplication = await ethers.getContractFactory("StableAssetApplication");
-  const application = await StableAssetApplication.attach('0x9aabd039fD0bF767Db26293a039998e85Bd31255');
-  await application.mint('0xd22f46Ba0425066159F828EFA5fFEab4DAeb9fd0', ['1000000000000000', '1000000000000000'], '0', {value: '1000000000000000'});
+  const application = await StableAssetApplication.attach('0x1aefabbe56db3c47f0824f4af3d74845e4bcd117');
+  await application.mint('0x774a36e77876eC9e887a07b2E53f81479d178532', ['1000000000000000', '1000000000000000'], '0', {value: '1000000000000000'});
 }
 
 // We recommend this pattern to be able to use async/await everywhere
