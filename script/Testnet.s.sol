5d9e672deb629c53da4a6594e27a354b74462d7c
zakrad <daniyal.rad@gmail.com>
2025-08-18 23:06:21 +0700

    feat: add sonic-testnet deployment

--- script/Testnet.s.sol
+++ script/Testnet.s.sol
@@ -32,43 +32,64 @@ contract Testnet is Deploy, Pool {
         deployZap();
 
         if (chainId == 57_054) {
+            // sonic testnet
+            address wS = 0x98e6a95a4B225b60456FC5dEDde3E4b8B7655C6B;
+            address stS = 0x6A074e2158e9C0f7A2e738f4919DC42316d153B0;
+            address wOS = 0xa22a772520aaaaCEaD463593871C34e6A4422c83;
+
             MockExchangeRateProvider wSToS = new MockExchangeRateProvider(1e18, 18);
             MockExchangeRateProvider stSToS = new MockExchangeRateProvider(1.01558e18, 18);
             MockExchangeRateProvider OSToS = new MockExchangeRateProvider(1.004739e18, 18);
 
-            MockToken wS = new MockToken("wS", "wS", 18);
-            MockToken stS = new MockToken("stS", "stS", 18);
-            MockToken OS = new MockToken("OS", "OS", 18);
+            uint256 amount = 10_000e18;
+            uint256 amountToMint = 100e18;
 
-            uint256 amount = 100e18;
-
-            MockToken(wS).mint(DEPLOYER, amount * 2);
+            MockToken(wS).mint(DEPLOYER, amount);
             MockToken(stS).mint(DEPLOYER, amount);
-            MockToken(OS).mint(DEPLOYER, amount);
+            MockToken(wOS).mint(DEPLOYER, amount);
 
-            (address spaToken, address pool, address wspaToken,,) =
-                createMockExchangeRatePool(address(wS), address(stS), address(wSToS), address(stSToS));
+            (
+                address wSstSSPAToken,
+                address wSstSPool,
+                address wSstSWSPAToken,
+                address wSstSRampAController,
+                address wSstSKeeper
+            ) = createMockExchangeRatePool(address(wS), address(stS), address(wSToS), address(stSToS));
 
-            initialMint(address(wS), address(stS), amount, amount, SelfPeggingAsset(pool));
+            initialMint(address(wS), address(stS), amountToMint, amountToMint, SelfPeggingAsset(wSstSPool));
 
-            (address spaToken2, address pool2, address wspaToken2,,) =
-                createMockExchangeRatePool(address(wS), address(OS), address(wSToS), address(OSToS));
-            initialMint(address(wS), address(OS), amount, amount, SelfPeggingAsset(pool2));
+            (
+                address wSwOSSPAToken,
+                address wSwOSPool,
+                address wSwOSWSPAToken,
+                address wSwOSRampAController,
+                address wSwOSKeeper
+            ) = createMockExchangeRatePool(address(wS), address(wOS), address(wSToS), address(OSToS));
+            initialMint(address(wS), address(wOS), amountToMint, amountToMint, SelfPeggingAsset(wSwOSPool));
 
             vm.writeJson(vm.serializeAddress("contracts", "Zap", zap), path);
             vm.writeJson(vm.serializeAddress("contracts", "Factory", address(factory)), path);
+            vm.writeJson(
+                vm.serializeAddress("contracts", "FactoryImplementation", address(factoryImplementation)), path
+            );
             vm.writeJson(vm.serializeAddress("contracts", "SelfPeggingAssetBeacon", selfPeggingAssetBeacon), path);
             vm.writeJson(vm.serializeAddress("contracts", "SPATokenBeacon", spaTokenBeacon), path);
             vm.writeJson(vm.serializeAddress("contracts", "WSPATokenBeacon", wspaTokenBeacon), path);
+            vm.writeJson(vm.serializeAddress("contracts", "RampAControllerBeacon", rampAControllerBeacon), path);
+            vm.writeJson(vm.serializeAddress("contracts", "KeeperImplementation", keeperImplementation), path);
             vm.writeJson(vm.serializeAddress("contracts", "wS", address(wS)), path);
             vm.writeJson(vm.serializeAddress("contracts", "stS", address(stS)), path);
-            vm.writeJson(vm.serializeAddress("contracts", "OS", address(OS)), path);
-            vm.writeJson(vm.serializeAddress("contracts", "wSstSPool", address(pool)), path);
-            vm.writeJson(vm.serializeAddress("contracts", "wSstSPoolSPAToken", spaToken), path);
-            vm.writeJson(vm.serializeAddress("contracts", "wSstSPoolWSPAToken", wspaToken), path);
-            vm.writeJson(vm.serializeAddress("contracts", "wSOSPool", address(pool2)), path);
-            vm.writeJson(vm.serializeAddress("contracts", "wSOSPoolSPAToken", spaToken2), path);
-            vm.writeJson(vm.serializeAddress("contracts", "wSOSPoolWSPAToken", wspaToken2), path);
+            vm.writeJson(vm.serializeAddress("contracts", "wOS", address(wOS)), path);
+            vm.writeJson(vm.serializeAddress("contracts", "wSstSPool", address(wSstSPool)), path);
+            vm.writeJson(vm.serializeAddress("contracts", "wSstSPoolSPAToken", wSstSSPAToken), path);
+            vm.writeJson(vm.serializeAddress("contracts", "wSstSPoolWSPAToken", wSstSWSPAToken), path);
+            vm.writeJson(vm.serializeAddress("contracts", "wSstSRampAController", wSstSRampAController), path);
+            vm.writeJson(vm.serializeAddress("contracts", "wSstSKeeper", wSstSKeeper), path);
+            vm.writeJson(vm.serializeAddress("contracts", "wSwOSPool", address(wSwOSPool)), path);
+            vm.writeJson(vm.serializeAddress("contracts", "wSwOSPoolSPAToken", wSwOSSPAToken), path);
+            vm.writeJson(vm.serializeAddress("contracts", "wSwOSPoolWSPAToken", wSwOSWSPAToken), path);
+            vm.writeJson(vm.serializeAddress("contracts", "wSwOSRampAController", wSwOSRampAController), path);
+            vm.writeJson(vm.serializeAddress("contracts", "wSwOSKeeper", wSwOSKeeper), path);
         }
 
         vm.stopBroadcast();
